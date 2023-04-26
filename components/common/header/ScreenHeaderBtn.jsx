1c42ec965c63acaf2053b1ae4855e0c2cf41f5f6
thalieskoda <thalieskoda@hotmail.com>
2023-04-26 13:22:09 -0400

    React-Native-Jobs

---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { TouchableOpacity, Image } from 'react-native';
+import styles from './screenheader.style'
+
+const ScreenHeaderBtn = ({iconURL, dimension, handlePress}) => {
+  return (
+   <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+    <Image source={iconURL}
+    resizeMode="cover"
+    style={styles.btnImg(dimension)}/>
+   </TouchableOpacity>
+  )
+}
+
+export default ScreenHeaderBtn
